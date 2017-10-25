750b20ae6fc8e02c85fdac801f89f3cf4872e981
root <yaro.tr@gmail.com>
2017-10-25 14:52:30 +0300

    Product Designer Order view

--- Block/Adminhtml/Order/View/Tab/Designer.php
+++ Block/Adminhtml/Order/View/Tab/Designer.php
@@ -5,6 +5,49 @@ namespace Develo\Designer\Block\Adminhtml\Order\View\Tab;
 class Designer extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder implements
     \Magento\Backend\Block\Widget\Tab\TabInterface{
     
+    protected $_designerOrder;
+    
+    protected $_designCartItemModel;
+    
+    protected $_productModel;
+    
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context, 
+        \Magento\Framework\Registry $registry, 
+        \Magento\Sales\Helper\Admin $adminHelper,
+        \Magento\Catalog\Model\ProductFactory $productModel,    
+        \Develo\Designer\Model\OrderFactory $designerOrder,    
+        \Develo\Designer\Model\CartitemFactory $designCartItemModel,    
+        array $data = array()
+    ) {
+        parent::__construct($context, $registry, $adminHelper, $data);
+        
+        $this->_designCartItemModel = $designCartItemModel;
+        $this->_designerOrder = $designerOrder;
+        $this->_productModel = $productModel;
+    }
+    
+    public function prepareConf($confStr) {
+        return $confStr ? json_decode($confStr) : [];
+    }
+    
+    public function getProduct($productId) {
+        return $this->_productModel->create()
+                ->load($productId);
+    }
+
+    public function getDesignerData() {
+        $order = $this->getOrder();
+        $collection = $this->_designCartItemModel->create()
+                ->getCollection();
+        
+        $collection->getSelect()->where('cart_quote_id=?', $order->getQuoteId());
+        return $collection;
+    }
+    
+    /**
+     * ######################## TAB settings #################################
+     */
     
     public function canShowTab(){
         return true;
@@ -19,7 +62,16 @@ class Designer extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder implem
     }
 
     public function isHidden(){
-        return false;
+        return $this->checkIsHidden();
+    }
+    
+    protected function checkIsHidden() {
+        $order = $this->getOrder();
+        $model = $this->_designerOrder->create()
+                ->load($order->getIncrementId(), 'magento_order_id');
+        if(!$model->getId()) {
+            return true;
+        }
     }
 
 }
